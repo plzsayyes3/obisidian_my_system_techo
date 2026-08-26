d3fc7a73d2ad0a1b5cb14a1c333c7b383d76be61
plzsayyes3 <102906997+plzsayyes3@users.noreply.github.com>
2026-08-26 12:05:10 +0900

    chore: add esbuild entrypoint

---
+++ esbuild.config.mjs
@@ -0,0 +1,24 @@
+import esbuild from "esbuild";
+import builtins from "builtin-modules";
+
+const args = process.argv.slice(2);
+const production = args.includes("production");
+const watch = args.includes("--watch");
+
+const context = await esbuild.context({
+  entryPoints: ["src/main.ts"],
+  bundle: true,
+  external: ["obsidian", ...builtins],
+  format: "cjs",
+  target: "es2020",
+  outfile: "main.js",
+  logLevel: "info",
+  sourcemap: !production,
+});
+
+if (watch) {
+  await context.watch();
+} else {
+  await context.rebuild();
+  await context.dispose();
+}
